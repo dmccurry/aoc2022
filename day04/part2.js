41345ee473958389deb8138fcef8de1741e9085c
David McCurry <dmccurry@cvent.com>
2022-12-04 06:29:31 -0400

    Combining splits

--- day04/part2.js
+++ day04/part2.js
@@ -4,14 +4,9 @@ fs.readFile('./input', 'utf-8', (_, data) => {
   const lines = data.split('\n');
   let overlaps = 0;
   lines.forEach(pair => {
-    const elfA = pair.split(',')[0];
-    const elfB = pair.split(',')[1];
-
-    const elfAStart = parseInt(elfA.split('-')[0]);
-    const elfAEnd = parseInt(elfA.split('-')[1]);
-    const elfBStart = parseInt(elfB.split('-')[0]);
-    const elfBEnd = parseInt(elfB.split('-')[1]);
-
+    const [elfA, elfB] = pair.split(',');
+    const [elfAStart, elfAEnd] = elfA.split('-').map(e => parseInt(e));
+    const [elfBStart, elfBEnd] = elfB.split('-').map(e => parseInt(e));
     if (elfBStart >= elfAStart && elfBStart <= elfAEnd) {
       overlaps++;
     } else if (elfAStart >= elfBStart && elfAStart <= elfBEnd) {
