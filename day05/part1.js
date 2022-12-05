34fed63e29dcc3486b5edf76cc042770a1ee8ba2
David McCurry <dmccurry@cvent.com>
2022-12-05 06:55:27 -0400

    Day 5

---
+++ day05/part1.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+fs.readFile('./input', 'utf-8', (_, data) => {
+
+  let [crates, steps] = data.split('\n\n');
+  let stacks = [];
+
+  while (crates.indexOf('    ') >= 0) {
+    crates = crates.replace(/    \[/ig, '[.] [');
+    crates = crates.replace(/\]    /ig, '] [.]');  
+  }
+  cratesSplit = crates.split('\n');
+
+  cratesSplit = cratesSplit.reverse();
+  let nCrates = cratesSplit.shift().replace(/ /ig, '').length;
+  for (let i = 0; i<nCrates; i++) {
+    stacks[i] = [];
+  }
+
+
+  for (let i=0; i<cratesSplit.length; i++) { 
+    const row = cratesSplit[i].split(' ').map(x => x.replace('[', '')).map(x => x.replace(']', ''));
+    
+    for (let j=0; j<row.length; j++) {
+      if (row[j] != '.') {
+        stacks[j].push(row[j]);
+      }
+    }
+  }
+  const stepsSplit = steps.split('\n');
+  for (let i=0; i<stepsSplit.length; i++) {
+    let [move, num, from, source, to, dest] = stepsSplit[i].split(' ');
+    num = parseInt(num);
+    source = parseInt(source) - 1;
+    dest = parseInt(dest) - 1;
+    
+    for (let j = 0; j < num; j++) {
+      stacks[dest].push(stacks[source].pop());
+    }
+  }
+
+  let solution = '';
+
+  for (let i=0; i<nCrates; i++) {
+    solution += stacks[i][stacks[i].length - 1];
+  }
+
+  console.log(`Solution is ${solution}`);
+  
+
+});
