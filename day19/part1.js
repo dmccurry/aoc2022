d3a31eafaab53f4162cb6bf3468c0d72faf8ce6b
David McCurry <dmccurry@cvent.com>
2022-12-19 21:28:11 -0400

    Day 19

---
+++ day19/part1.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+const blueprints = [];
+
+fs.readFile('./input', 'utf-8', (_, data) => {
+  const lines = data.split('\n');
+  lines.forEach(line => {
+    const [blueprint, ore, clay, obs, obs2, geode, geode2] = line.match(new RegExp(/(\d+)/, "ig")).map(n => parseInt(n));
+    blueprints.push({
+      id: blueprint,
+      ore,
+      clay,
+      obsidian: {
+        ore: obs,
+        clay: obs2
+      },
+      geode: {
+        ore: geode,
+        obsidian: geode2
+      },
+      answer: -Infinity,
+      quality: -Infinity
+    });
+  });
+  blueprints.forEach(blueprint => {
+    step(blueprint, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+  });
+  let totalQuality = 0;
+  blueprints.forEach(blueprint => {
+    blueprint.quality = blueprint.id * blueprint.answer;
+    totalQuality += blueprint.quality;
+  });
+  console.log(`Solution is ${totalQuality}`);
+});
+
+const step = (blueprint, oreRobots, clayRobots, obsidianRobots, geodeRobots, ore, clay, obsidian, geode, minute) => {
+
+  // base case
+  if (minute == 23) {
+    // add a last geode, that's all we care about
+    geode += geodeRobots;
+    if (geode > blueprint.answer) {
+      blueprint.answer = geode;
+    }
+    return;
+  }
+
+  // ugh we need to optimize
+  let tmpRobots = geodeRobots;
+  let tmpGeodes = geode;
+  for (let i=minute; i<24; i++) {
+    tmpGeodes+= tmpRobots;
+    tmpRobots++;
+  }
+  if (tmpGeodes < blueprint.answer) return;
+
+  const canBuildGeode = ore >= blueprint.geode.ore && obsidian >= blueprint.geode.obsidian;
+  const canBuildObsidian = ore >= blueprint.obsidian.ore && clay >= blueprint.obsidian.clay;
+  const canBuildClay = ore >= blueprint.clay;
+  const canBuildOre = ore >= blueprint.ore;
+
+  // always build a geode if you can
+  if (canBuildGeode) {
+    ore = ore - blueprint.geode.ore;
+    obsidian = obsidian - blueprint.geode.obsidian;
+  } else {
+    // figure out what we should build
+    const shouldBuildObsidian = obsidianRobots < blueprint.geode.obsidian;
+    const shouldBuildClay = clayRobots < blueprint.obsidian.clay;
+    const shouldBuildOre = oreRobots < Math.max(blueprint.ore, blueprint.clay, blueprint.obsidian.ore, blueprint.geode.ore);
+    if (canBuildObsidian && shouldBuildObsidian) {
+      step(blueprint, oreRobots, clayRobots, obsidianRobots + 1, geodeRobots, ore + oreRobots - blueprint.obsidian.ore, clay + clayRobots - blueprint.obsidian.clay, obsidian + obsidianRobots, geode + geodeRobots, minute + 1);
+    }
+    if (canBuildClay && shouldBuildClay) {
+      step(blueprint, oreRobots, clayRobots + 1, obsidianRobots, geodeRobots, ore + oreRobots - blueprint.clay, clay + clayRobots, obsidian + obsidianRobots, geode + geodeRobots, minute + 1);
+    }
+    if (canBuildOre && shouldBuildOre) {
+      step(blueprint, oreRobots + 1, clayRobots, obsidianRobots, geodeRobots, ore + oreRobots - blueprint.ore, clay + clayRobots, obsidian + obsidianRobots, geode + geodeRobots, minute + 1);
+    }
+  }
+  ore += oreRobots;
+  clay+= clayRobots;
+  obsidian += obsidianRobots;
+  geode += geodeRobots;
+
+  if (canBuildGeode) {
+    geodeRobots++;
+  }
+
+  step(blueprint, oreRobots, clayRobots, obsidianRobots, geodeRobots, ore, clay, obsidian, geode, minute+1);
+}
